1577be770cf8cb511f471ac8757487b3bfb4136a
Kirsty <kirsty@tangledfrog.com>
2018-01-12 17:52:27 +0000

    Added paddle movement

--- practical_1/main.cpp
+++ practical_1/main.cpp
@@ -8,11 +8,21 @@ using namespace std;
 const int gameWidth = 800;
 const int gameHeight = 600;
 
+const  Keyboard ::Key  controls [4] = {
+    Keyboard ::A,
+    Keyboard ::Z,
+    Keyboard ::Up,
+    Keyboard ::Down
+};
+
+
+
 CircleShape ball;
 const float ballRadius = 10.f;
 
 RectangleShape paddles[2];
 const Vector2f paddleSize(25.f, 100.f);
+const  float  paddleSpeed = 400.f;
 
 void load() {
 
@@ -24,29 +34,47 @@ void load() {
 
     paddles[0].setSize(paddleSize);
     paddles[0].setOrigin(paddleSize / 2.f);
-    paddles[0].setPosition(25.f, 300.f);
+    paddles[0].setPosition(10 + (paddleSize.x / 2), gameHeight / 2);
     paddles[0].setFillColor(sf::Color::White);
 
     paddles[1].setSize(paddleSize);
     paddles[1].setOrigin(paddleSize / 2.f);
-    paddles[1].setPosition(gameWidth - 25.f, gameHeight / 2);
+    paddles[1].setPosition(gameWidth - 10 - (paddleSize.x / 2), gameHeight / 2);
     paddles[1].setFillColor(sf::Color::White);
+
 
 }
 
 void update(RenderWindow &window) {
 
-    // get time since last called
     static sf::Clock clock; // should this go here?
     const float dt = clock.restart().asSeconds();
 
-    // check events
     sf::Event event;
     while (window.pollEvent(event))
     {
         if (event.type == sf::Event::Closed)
             window.close();
     }
+
+    float direction = 0.f;
+    if (Keyboard::isKeyPressed(controls[0])) {
+        direction--;
+    }
+    if (Keyboard::isKeyPressed(controls[1])) {
+        direction++;
+    }
+    paddles[0].move(0, direction * paddleSpeed * dt);
+
+    float direction2 = 0.f;
+    if (Keyboard::isKeyPressed(controls[2])) {
+        direction2--;
+    }
+    if (Keyboard::isKeyPressed(controls[3])) {
+        direction2++;
+    }
+    paddles[1].move(0, direction2 * paddleSpeed * dt);
+
 }
 
 void render(RenderWindow &window) {
