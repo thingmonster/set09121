02fe96b2278eda0c00685040f9ba9a70c148169c
Kirsty Williams <40129403@live.napier.ac.uk>
2018-01-20 15:57:06 +0000

    Added invader movement

--- practical_2/ship.cpp
+++ practical_2/ship.cpp
@@ -1,11 +1,13 @@
 
+#include <iostream>
 #include "game.h"
 #include "ship.h"
 
 using namespace sf;
 using namespace std;
 
-
+bool Invader::direction;
+float Invader::speed;
 
 Ship::Ship() {};
 
@@ -24,8 +26,34 @@ Invader::Invader() : Ship() {}
 Invader::Invader(sf::IntRect ir, sf::Vector2f pos) : Ship(ir) {
 	setOrigin(16, 16);
 	setPosition(pos);
+	direction = true;
 }
 
 void Invader::Update(const float &dt) {
+	
 	Ship::Update(dt);
+	
+	move(dt * (direction ? 1.0f : -1.0f) * speed, 0);
+	
+	if ((direction && getPosition().x > gameWidth - 16) || (!direction && getPosition().x < 16)) {
+		cout << "test";
+		if (direction) {
+			direction = false;
+		} else {
+			direction = true;
+		}
+	}
+
+	float newPos;
+	if (direction) {
+		newPos = 24;
+	} else {
+		newPos = -24;
+	}
+	
+	for (int i = 0; i < ships.size(); i++) {
+		ships[i]->move(newPos, 0);
+	}
 }
+
+
--- practical_2/ship.h
+++ practical_2/ship.h
@@ -18,7 +18,21 @@ public:
 	Invader(sf::IntRect ir, sf::Vector2f pos);
 	Invader();
 	void Update(const float &dt) override;
-	// void Update(const float &dt);
-	// static bool direction;
-	// static float speed;
+	static bool direction;
+	static float speed;
 };
+
+// void Invader::Update(const float &dt) {
+	
+	// Ship::Update(dt);
+	
+	// move(dt * (direction ? 1.0f : -1.0f) * speed, 0);
+	
+	// if ((direction && getPosition().x > gameWidth - 16) || (!direction && getPosition().x < 16)) {
+		// direction = !direction;
+		// for (int i = 0; i < 1; ++i) {
+			// ships[i]->move(0, 24);
+		// }
+	// }
+// }
+extern std::vector<Ship *> ships;
